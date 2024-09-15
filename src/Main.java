88884c82ac24af6bba494497bc1c5f3523b40a4a
django <pitbull.09@mail.ru>
2024-09-15 11:12:34 +0300

    8

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,20 @@
-
-    public static int percentageCalculation(int amount, int percent) {
-        return amount / 100 * percent;
-    }
-
+public class Main {
     public static void task1(int iniDep) {
-        int total = 0;
-        int month = 1;
-        while (total <= 2_459_000) {
-            total += iniDep;
-            total += total / 100;
-            System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
-            month++;
+    int desiredSum = 2_495_000;
+    int sum = 0;
+    int amount = 15_000;
+        double percent = 10/100;
+        int month = 0;
+                while (sum < desiredSum) {
+                    sum += amount;
+                    sum = (int) (sum * (1 + percent));
+                    month++;
+                System.out.println("Месяц " + month + ", сумма накоплений равна " + sum + " рублей");
         }
     }
 
     public static void task2() {
-        int i = 1;
+        int i = 0;
         int j = 10;
         while (i <= 10) {
             System.out.print(i + " ");
@@ -32,41 +31,37 @@
         int population = 12_000_000;
         int birthRate = 17;
         int deathRate = 8;
-        int years = 10;
-        for (int year = 1; year <= years; year++) {
-            int births = (population * birthRate) / 1000;
-            int deaths = (population * deathRate) / 1000;
-            population += (births - deaths);
+        int currentYears = 2024;
+        for (int year = currentYears; year < currentYears + 100; year++) {
+            population += birthRate / 1000 - population * deathRate / 1000;
             System.out.println("Год " + year + ", численность населения составляет " + population);
         }
     }
 
     public static void task4(int iniDep) {
-        int percent = 7;
+        int percent = 7/100;
         int goal = 12_000_000;
-        int result = 0;
+        int sum = amount;
         int month = 0;
-        do {
-            result += iniDep;
-            result += percentageCalculation(iniDep, percent);
+        while (sum < amount) {
+            sum = (int) (sum * (1 + percent);
             month++;
-            System.out.println("Месяц " + month + ". Накоплено: " + result);
-        } while (result <= goal);
+            System.out.println("Месяц " + month + ". Накоплено: " + sum + "рублей");
+        }
     }
 
     public static void task5(int iniDep) {
-        int percent = 7;
+        int percent = 7/100;
         int goal = 12_000_000;
-        int result = 0;
+        int sum = amount;
         int month = 0;
-        do {
-            result += iniDep;
-            result += percentageCalculation(iniDep, percent);
+        while (sum < amount) {
+            sum = (int) (sum * (1 + percent);
             month++;
-            if (month % 6 == 0 || result > goal) {
-                System.out.println("Месяц " + month + ". Накоплено: " + result);
-            }
-        } while (result <= goal);
+            if (month % 6 ==0) {
+            System.out.println("Месяц " + month + ". Накоплено: " + sum + "рублей");
+        }
+    }
     }
 
     public static void task6(int iniDep) {
@@ -96,14 +91,18 @@
 
     public static void task8() {
         int emergencePeriod = 79;
-        int currentYear = 2024;
-        int lastYears = currentYear - 200;
-        int nextYears = currentYear + 100;
-        for (int i = 0; i <= nextYears; i += emergencePeriod) {
-            if (i > lastYears && i < nextYears) {
-                System.out.println(i);
+        int startSeeing =0;
+        int start = currentYear - 200;
+        int end = currentYear + 100;
+        for (int year = start; year < end; year++) {
+            if (year > start) {
+                System.out.println(year);
             }
         }
-
+            for (int year = start; year < end; year++) {
+                if (year % emergencePeriod == startSeeing) {
+                    System.out.println(year);
+                }
+            }
     }
 }
